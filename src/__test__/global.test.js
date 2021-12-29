f2968da380c253235146a13451bf227c0ef984e6
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-29 11:23:52 -0600

    test promise and async/await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,30 @@ test('Probar un callback', () => {
         expect(str).toBe('aloH')
     })
 })
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if(!str){
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('Probar una promesa', () => {
+    reverseString2('Hola')
+    .then(string => {
+        expect(string).toBe('aloH')
+    })
+})
+
+test('Probar async/await', async () => {
+    const str = await reverseString2('Hola')
+    expect(str).toBe('aloH')
+
+})
+
+// afterEach(() => console.log('Despues de cada prueba'))
+// afterAll(() => console.log('Despues de todas las pruebas'))
+// beforeEach(() => console.log('Antes de cada prueba'))
+// beforeAll(() => console.log('Antes de todas las pruebas'))
